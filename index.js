cc805bdeb5de515e0a83b23d9837fbbce2ca27be
FirstDot <shinkyle9102@gmail.com>
2021-07-15 12:58:03 +0900

    2nd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 var server = http.createServer(app);
 
 app.get('/', function(req, res){
-    res.send('I like myunggeun!');
+    res.send('I like myunggeunshin!');
 });
 
 app.get('/start', function(req, res){
